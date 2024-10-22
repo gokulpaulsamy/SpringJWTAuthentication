14ddbb0fdc9c8c662a49570daa2f81ffb0314630
gokulpaulsamy <gokul.p@bthinq.com>
2024-10-22 20:00:06 +0530

    'initial'

---
+++ src/test/java/com/example/demo/JwtTokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
